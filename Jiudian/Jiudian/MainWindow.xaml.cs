73ec1de7a6af08c9f3c2c3e2657080fdf0a0a5f6
Dai Li <d.li@simplylive.tv>
2020-08-01 22:20:50 +0800

    adf

--- Jiudian/Jiudian/MainWindow.xaml.cs
+++ Jiudian/Jiudian/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,36 @@ namespace Jiudian
         public MainWindow()
         {
             InitializeComponent();
+
+            this.topButton.exitBtn.Click += exit;
+            this.addBtn.Click += AddBtn_Click;
+            //this.kongweishu.Text = "空位数:15";
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ConnectDB()
+        {
+            string sql = "Data Source=DESKTOP-HMVR0JT;Initial Catalog=property;User ID=sa;Password=123456";
+
+                       //string sql = "server=JOY;database=VofinePearl;uid=sa;pwd=123456";//连接字符串
+            SqlConnection sqlcon = new SqlConnection(sql);//
+            string sql1 = "select parked_id from zk_platelist";
+            SqlDataAdapter sqlada = new SqlDataAdapter(sql1, sqlcon);
+            DataSet ds = new DataSet();
+            ds.Clear();
+            DataTable table1 = new DataTable();
+            sqlada.Fill(ds, "table1");
+            dataGrid1.DataContext = ds;
+        }
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            this.topButton.infoTextBox.Text = "aaaaa"+DateTime.Now.ToString();
+            ConnectDB();
+
         }
     }
 }
